965328bd6e5b58f7d8640200c7f66132665a55e4
lucabazzocchi <luca.bazzocchi.stud@ispascalcomandini.it>
2025-10-29 10:20:35 +0100

    ok

---
+++ SmartHouse/src/BlaisePascal.SmartHouse.Domain/SmartHouse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Domain
+{
+    internal class SmartHouse
+    {
+        public SmartHouse() { }
+        //push di prova
+        //pushhh
+    }
+}
