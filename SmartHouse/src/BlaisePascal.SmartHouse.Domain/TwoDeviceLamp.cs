3c0ae7bde30d988fdf30b85faf22d8fcc35b04cf
MattiaBalzani <mattia.balzani.stud@ispascalcomandini.it>
2025-11-05 11:00:30 +0100

    Inizialized  TwoLampDevice

--- SmartHouse/src/BlaisePascal.SmartHouse.Domain/TwoDeviceLamp.cs
+++ SmartHouse/src/BlaisePascal.SmartHouse.Domain/TwoDeviceLamp.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BlaisePascal.SmartHouse.Domain
 {
     public class TwoDeviceLamp
+
     {
-        public void TurnOnLamp()
+        public const int MaxBrightness = 100;
+        public bool IsOn { get; private set; }
+        public int Brightness { get; private set; }
+        public Lamp _lamp { get; private set; }
+        
+        public TwoDeviceLamp(int _brightness, Lamp lamp)
         {
-            Lamp.
+            Brightness = _brightness;
+            _lamp = new Lamp();
+        }
+        public void TurnOnOffLamp()
+        {
+            _lamp.TurnOnOff();
         }
     }
 }
