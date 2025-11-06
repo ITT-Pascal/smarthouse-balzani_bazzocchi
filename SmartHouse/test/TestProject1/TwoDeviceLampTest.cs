d82653f2cf68800a8fe9913e559e79662d66f05e
MattiaBalzani <mattia.balzani.stud@ispascalcomandini.it>
2025-11-06 22:36:36 +0100

    Added TwoDeviceLamp test.

--- SmartHouse/test/TestProject1/TwoDeviceLampTest.cs
+++ SmartHouse/test/TestProject1/TwoDeviceLampTest.cs
@@ -48,8 +48,86 @@ public class TwoDeviceLampTest // foca
     [Fact]
     public void TurnOnOffBoth_WhenEcoLampAndLampAreOffTurnItsOn()
     {
-        
+        TwoDeviceLamp newTwoDeviceLamp = new TwoDeviceLamp();
+
+        newTwoDeviceLamp.TurnOnOffBoth();
+
+        Assert.True(newTwoDeviceLamp.IsEcoLampOn());
+        Assert.True(newTwoDeviceLamp.IsLampOn());
     }
+
+    [Fact]
+    public void TurnOffBoth_WhenEcoLampIsOnAndLampIsOffTurnEcoLampOffAndTurnLampOn()
+    {
+        TwoDeviceLamp newTwoDeviceLamp = new TwoDeviceLamp();
+
+        newTwoDeviceLamp.TurnOnOffEcoLamp();
+        newTwoDeviceLamp.TurnOnOffBoth();
+
+        Assert.False(newTwoDeviceLamp.IsEcoLampOn());
+        Assert.True(newTwoDeviceLamp.IsLampOn());
+    }
+
+    [Fact]
+    public void TurnOffBoth_WhenEcoLampIsOffAndLampIsOnTurnEcoLampOnAndTurnLampOff()
+    {
+        TwoDeviceLamp newTwoDeviceLamp = new TwoDeviceLamp();
+
+        newTwoDeviceLamp.TurnOnOffLamp();
+        newTwoDeviceLamp.TurnOnOffBoth();
+
+        Assert.True(newTwoDeviceLamp.IsEcoLampOn());
+        Assert.False(newTwoDeviceLamp.IsLampOn());
+    }
+
+    [Fact]
+    public void TurnOnOffBoth_WhenEcoLampAndLampAreOnTurnItsOff()
+    {
+        TwoDeviceLamp newTwoDeviceLamp = new TwoDeviceLamp();
+
+        newTwoDeviceLamp.TurnOnOffEcoLamp();
+        newTwoDeviceLamp.TurnOnOffLamp();
+        newTwoDeviceLamp.TurnOnOffBoth();
+
+        Assert.False(newTwoDeviceLamp.IsEcoLampOn());
+        Assert.False(newTwoDeviceLamp.IsLampOn());
+    }
+
+    [Fact]
+    public void ChangeEcoLampBrightness_IfYouChangeTheEcoLampBrightnessTo17ThenTheEcoLampBrightnessIsAt17()
+    {
+        TwoDeviceLamp newTwoDeviceLamp = new TwoDeviceLamp();
+
+        newTwoDeviceLamp.TurnOnOffEcoLamp();
+        newTwoDeviceLamp.ChangeEcoLampBrightness(17);
+
+        Assert.Equal(17, newTwoDeviceLamp._ecoLamp.Brightness);
+    }
+
+    [Fact]
+    public void ChangeLampBrightness_IfYouChangeTheLampBrightnessTo3ThenTheLampBrightnessIsAt3()
+    {
+        TwoDeviceLamp newTwoDeviceLamp = new TwoDeviceLamp();
+
+        newTwoDeviceLamp.TurnOnOffLamp();
+        newTwoDeviceLamp.ChangeLampBrightness(3);
+
+        Assert.Equal(3, newTwoDeviceLamp._lamp.Brightness);
+    }
+
+    [Fact]
+    public void ChangeEcoLampAndLampBrightness_IfYouChangeEcoLampAndLampBrightnessTo20ThenEcoLampAndLampBrightnessIsAt20()
+    {
+        TwoDeviceLamp newTwoDeviceLamp = new TwoDeviceLamp();
+
+        newTwoDeviceLamp.TurnOnOffLamp();
+        newTwoDeviceLamp.TurnOnOffEcoLamp();
+        newTwoDeviceLamp.ChangeEcoLampAndLampBrightness(20);
+
+        Assert.Equal(20, newTwoDeviceLamp._lamp.Brightness);
+        Assert.Equal(20, newTwoDeviceLamp._ecoLamp.Brightness);
+    }
+
 
 
 
