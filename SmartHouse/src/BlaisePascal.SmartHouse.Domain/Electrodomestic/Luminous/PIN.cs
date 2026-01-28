0df8bf25e23f974eb6e04a0f4ff138141548caf9
MattiaBalzani <mattia.balzani.stud@ispascalcomandini.it>
2026-01-28 10:59:15 +0100

    Added PIN record

---
+++ SmartHouse/src/BlaisePascal.SmartHouse.Domain/Electrodomestic/Luminous/PIN.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Domain.Electrodomestic.Luminous
+{
+    public class PIN
+    {
+        public string SecurityCode { get; init; }
+
+        public PIN(string securityCode)
+        {
+            if (string.IsNullOrWhiteSpace(securityCode) || securityCode.Length != 4 || !securityCode.All(char.IsDigit))
+                throw new ArgumentException(nameof(securityCode), "Il codice di sicurezza inserito non è valido.");
+            SecurityCode = securityCode;
+        }
+    }
+
+}
